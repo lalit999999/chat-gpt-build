9207f0852674dfe5eb329a3b273c077cd694ed5c
Lalit gurjar <gujarlalit79@gmail.com>
2026-07-25 19:22:55 +0530

    feat: implement message handling actions including create, update, list, and delete functionalities

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,27 @@
+"use client";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
+import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-   <div>
-    <h1>Hello World</h1>
-    <ModeToggle />
-    <UserButton/>
-   </div>
-  );
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["data"],
+    queryFn: async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>Something went wrong</h1>;
+  }
+
+  return <div>{JSON.stringify(data)}</div>;
 }
