2f3d83f364dd1f411c3512a90dc8dfcad4fad480
Lalit gurjar <gujarlalit79@gmail.com>
2026-07-25 19:50:22 +0530

    feat: implement conversation and chat components with sidebar and message input

---
+++ app/(root)/c/[id]/page.tsx
@@ -0,0 +1,29 @@
+// import { loadChatMessages } from '@/features/ai/actions/chat-store';
+import { getConversation } from "@/features/conversation/actions/consversation-action";
+import { ActionDidRevalidateDynamicOnly } from "next/dist/shared/lib/action-revalidation-kind";
+// import { ConversationView } from '@/features/conversation/components/conversation-view';
+import { notFound } from "next/navigation";
+import React from "react";
+
+type ConversationPageProps = {
+  params: Promise<{ id: string }>;
+};
+
+/**
+ * Conversation page — loads messages and renders the chat UI for a given ID.
+ */
+const page = async ({ params }: ConversationPageProps) => {
+  const { id } = await params;
+
+  // try {
+  //   await getConversation(id)
+  // } catch (error) {
+  //   notFound()
+  // }
+
+  // const initialMessages = await loadChatMessages(id);
+
+  return <div>Page ${id}</div>;
+};
+
+export default page;
